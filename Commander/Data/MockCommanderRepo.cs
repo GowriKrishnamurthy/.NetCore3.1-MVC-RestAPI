26acf5010ba6a472590815b211ec4837a00ecc0e
Gowri Krishnamurthy <kr_gowri@yahoo.com>
2020-09-20 22:02:29 +1000

    Adding model and data for commands

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Boil an egg", CommandLine ="Boil water", Platform="Kettle & Pan"},
+                new Command{Id=1, HowTo="Cut bread", CommandLine ="Get a knife", Platform="knife & chopping board"},
+                new Command{Id=2, HowTo="Make cup of tea", CommandLine ="Place teabag in cup", Platform="Kettle & cup"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", CommandLine = "Boil water", Platform = "Kettle & Pan" };
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
