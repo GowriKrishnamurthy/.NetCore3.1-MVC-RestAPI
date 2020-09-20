ec240682df1e345f89df55830e03ce6693a69d93
Gowri Krishnamurthy <kr_gowri@yahoo.com>
2020-09-20 22:02:35 +1000

    Adding model and data for commands

---
+++ Commander/Data/ICommanderRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public interface ICommanderRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+        void CreateCommand(Command command);
+        void UpdateCommand(Command command);
+        void DeleteCommand(Command command);
+    }
+}
