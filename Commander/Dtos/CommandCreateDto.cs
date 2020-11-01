7331d90ac54ee1807e12044c3fdce9fdf6f415ef
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-11-01 16:19:03 +1100

    Added Annotations to DTO to avoid 500 Errors

--- Commander/Dtos/CommandCreateDto.cs
+++ Commander/Dtos/CommandCreateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,14 @@ namespace Commander.Dtos
 {
     public class CommandCreateDto
     {
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
-        public string CommandLine { get; set; }        
+        
+        [Required] 
+        public string CommandLine { get; set; }
+        
+        [Required] 
         public string Platform { get; set; }
     }
 }
